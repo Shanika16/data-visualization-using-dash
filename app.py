2254fc93bb12d2691529593b3ca185d385214dab
Shanika16 <148519555+Shanika16@users.noreply.github.com>
2024-01-26 17:37:34 -0700

    app.py

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 import streamlit as st
-import pandas as pd
 import plotly.express as px
+import pandas as pd
 
 # Read in the data
 data = pd.read_csv("precious_metals_prices_2018_2021.csv")
 data["DateTime"] = pd.to_datetime(data["DateTime"], format="%Y-%m-%d")
 
-# Sidebar for filters
-st.sidebar.header("Filter Options")
-metal = st.sidebar.selectbox("Select Metal", data.columns[1:], index=0)
-
+# Sidebar
+st.sidebar.title("Precious Metal Prices")
+metal = st.sidebar.selectbox("Metal", data.columns[1:], index=0)
 date_range = st.sidebar.date_input("Select Date Range", [data["DateTime"].min().date(), data["DateTime"].max().date()])
 
+# Filter data based on user input
 filtered_data = data.loc[(data["DateTime"] >= date_range[0]) & (data["DateTime"] <= date_range[1])]
 
-# Plot the data
-st.title("Precious Metal Prices 2018-2021")
-st.header("Metal Prices Over Time")
+# Create a plotly plot
 fig = px.line(
     filtered_data,
+    title=f"Precious Metal Prices {date_range[0]} - {date_range[1]}",
     x="DateTime",
     y=[metal],
-    title=f"{metal} Prices 2018-2021",
-    labels={"DateTime": "Date", metal: "Price (USD/oz)"},
-    line_shape="linear",
-    template="plotly_dark",
     color_discrete_map={
         "Platinum": "#E5E4E2",
         "Gold": "gold",
@@ -35,4 +30,21 @@ fig = px.line(
         "Ruthenium": "#C9CBC8"
     }
 )
+
+fig.update_layout(
+    template="plotly_dark",
+    xaxis_title="Date",
+    yaxis_title="Price (USD/oz)",
+    font=dict(
+        family="Verdana, sans-serif",
+        size=18,
+        color="white"
+    ),
+)
+
+# Display the plot
 st.plotly_chart(fig)
+
+# Display additional information
+st.title("Precious Metal Prices")
+st.markdown("The cost of precious metals between 2018 and 2021.")
